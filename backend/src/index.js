f92a1faada529d52bf90f8adb4dd3c8f66ea0346
Vidisha Deswal <vidishadeswal@Vidishas-MacBook-Air.local>
2025-12-09 19:07:25 +0530

    Configure for production deployment - Vercel & Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,7 @@ app.use(
     origin: (origin, callback) => {
       const allowedOrigins = [
         process.env.FRONTEND_URL || "http://localhost:5173",
+        "https://o-auth-airtable.vercel.app",
         "http://localhost:3000",
         "http://localhost:5173",
       ];
